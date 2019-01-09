06b9c458bc124dd3f23ba32c1bc0002997d7bf14
Cletinic <chanelletinic@gmail.com>
2019-01-09 16:49:51 +0200

    displays 1 output's title, image, year, link. has an error though

--- index.php
+++ index.php
@@ -7,17 +7,16 @@
 		href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" 
 		integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" 
 		crossorigin="anonymous">
-	<script 
-		src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" 
-		integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" 
-		crossorigin="anonymous">
-	</script>
 	<script
 		src="https://code.jquery.com/jquery-3.3.1.js"
 		integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60="
 		crossorigin="anonymous">
 	</script>
-
+	<script 
+		src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" 
+		integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" 
+		crossorigin="anonymous">
+	</script>
 	<link 
 		rel="stylesheet" 
 		href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" 
@@ -97,31 +96,47 @@
 				<button id="searchText" type="submit"><i class="fa fa-search"></i></button> 
 			</form> -->
 			<div id="searchForm" class="form-inline my-2 my-lg-0">
-				<input class="form" type="search" placeholder="Search" style="border-radius: 0%;">
+				<input id="searchbar" class="form" type="search" placeholder="Search" style="border-radius: 0%;" onkeyup="getMovies(document.getElementById('searchbar').value)">
 			</div>
 		</div>
 	</div>
-	<div class="container">
+	<div class="card border-info mb-3" id="result">  
+		</div>
+	<!-- <div class="container">
 		<div id="movies" class="row"></div>
-	</div>
+	</div> -->
 </body>
 </html>
 <script type="text/javascript">
-	$(document).ready(function()
-	{
-		$('#searchForm').on('submit', function(e)
-		{
-			let searchText = $('#searchText').val();
-			getMovies(searchText);
-			e.preventDefault();
-		});
-	});
+	// $(document).ready(function()
+	// {
+	// 	$('#searchForm').on('submit', function(e)
+	// 	{
+	// 		let searchText = $('#searchText').val();
+	// 		getMovies(searchText);
+	// 		e.preventDefault();
+	// 	});
+	// });
 
 	//https://www.youtube.com/watch?v=67eJTr6_ylY
 	//https://www.youtube.com/watch?v=aMKf3su6TjI
 	//https://www.youtube.com/watch?v=bpHtxx_wmqw
 	function getMovies(e)
 	{
-		$.get("http://www.omdbapi.com/?s="+e+"apikey=1f18a935")
+		$.get("http://www.omdbapi.com/?s="+e+"&apikey=1f18a935",function(rawdata)
+		{
+			var rawstring = JSON.stringify(rawdata);
+			data = JSON.parse(rawstring)
+			var title = data.Search[0].Title; 
+			var year = data.Search[0].Year;
+			var imdbURL = "https://www.imdb.com/title/"+data.Search[0].imdbID+"/";
+			var posterURL = data.Search[0].Poster;
+			document.getElementById('result').innerHTML="<h1>"+title+"</h1><br><img src='"+posterURL+"'/><br><p>Year Released: "+year+"</p><br><p>IMDb: <a href="+imdbURL+">"+imdbURL+"</a></p>";
+		});
 	}
+
+	// $("#searchbar".value).keyup(function() 
+	// {
+	// 	alert( "Handler for .keyup() called." );
+	// });
 </script>
